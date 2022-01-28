750c17933f30aab082ffa09ca0bda0448db2bf6e
Lorescovich <lhorescovich@gmail.com>
2022-01-28 15:06:59 -0300

    Creo Controllers y Base de Datos MySQL

--- Models/SmartphoneContext.cs
+++ Models/SmartphoneContext.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Smartphones.Models;
 
 namespace Smartphones.Models
 {
-    public class SmartphoneContext
+    public class SmartphoneContext : DbContext
     {
+        public SmartphoneContext(DbContextOptions<SmartphoneContext> options) : base(options)
+        {
+        }
+        public DbSet<App> App { get; set; }
+        public DbSet<Instalacion> Instalacion { get; set; }
+        public DbSet<Operario> Operario { get; set; }
+        public DbSet<Smartphones.Models.Sensor> Sensor { get; set; }
+        public DbSet<Smartphones.Models.Telefono> Telefono { get; set; }
 
-    }
+
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }*/
+
+
+
+     }
 }
--- Startup.cs
+++ Startup.cs
@@ -5,10 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Smartphones.Models;
+using System.Text.Json.Serialization;
 
 namespace Smartphones
 {
@@ -24,8 +27,11 @@ namespace Smartphones
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            //.AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.AddDbContext<SmartphoneContext>(opt => opt.UseMySQL("server=localhost;database=smartphonedb;user=root;password=root"));
+
 
         }
 
